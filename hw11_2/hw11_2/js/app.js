1aedfe175cc1b753a93fe75bbababdef10bc204d
Ali <112693871+mavlianov20001@users.noreply.github.com>
2023-01-28 17:41:06 +0600

    hw11_2

---
+++ hw11_2/hw11_2/js/app.js
@@ -0,0 +1,111 @@
+const tabs = document.querySelectorAll(".tabheader__item")
+const tabsParent = document.querySelector(".tabheader__items")
+const tabsContent = document.querySelectorAll(".tabcontent")
+
+
+const hideTabContent = () => {
+    tabsContent.forEach((item) => {
+        item.style.display = "none"
+    })
+    tabs.forEach((item) => {
+        item.classList.remove("tabheader__item_active")
+    })
+}
+
+
+const showTabContent = (i = 3) => {
+    tabsContent[i].style.display = "block"
+    tabs[i].classList.add("tabheader__item_active")
+}
+
+hideTabContent()
+showTabContent()
+
+//1
+let q = 0;
+function slider () {
+     const sl = setInterval(()=>{
+          q++
+          if (q > 3) {
+               q = 0
+          }
+          hideTabContent()
+          showTabContent(q)
+     }, 2000)
+}
+slider()
+
+
+tabsParent.addEventListener("click", (e)=>{
+    const target = e.target
+
+    if(target.classList.contains("tabheader__item")){
+        tabs.forEach((item, i) => {
+            if(target === item) {
+                hideTabContent()
+                showTabContent(i)
+                q = i
+            }
+        })
+    }
+})
+
+const modalTrigger = document.querySelector(".btn_white")
+const modal = document.querySelector(".modal")
+const modalCloseBtn = document.querySelector(".modal__close")
+
+const openModal = () => {
+    modal.classList.add("show")
+    modal.classList.remove("hide")
+    document.body.style.overflow = "hidden"
+}
+
+modalTrigger.addEventListener("click", openModal)
+
+const closeModal = () => {
+    modal.classList.add("hide")
+    modal.classList.remove("show")
+    document.body.style.overflow = ""
+}
+
+modalCloseBtn.addEventListener("click", closeModal)
+
+modal.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("modal")){
+        closeModal()
+    }
+})
+//2
+const int = () => {
+    let page = document.documentElement
+    if (page.scrollTop + page.clientHeight >= page.scrollHeight) {
+        openModal()
+        document.body.style.overflow = "hidden"
+    }
+}
+
+window.addEventListener("scroll", int)
+
+
+//dz5
+const forms = document.querySelectorAll('form')
+
+const postData = (form) =>{
+    form.addEventListener('input', (e) =>{
+        e.preventDefault()
+        const req = new XMLHttpRequest()
+        req.open('POST', 'server.php')
+        req.setRequestHeader('Content-type', 'application/json')
+        const formData = new FormData(form)
+        const obj = {}
+        formData.forEach((item, name)=>{
+            obj[name] = item
+        })
+        console.log(obj)
+        const json = JSON.stringify(obj)
+        req.send(json)
+    })
+}
+forms.forEach((form)=>{
+    postData(form)
+})
